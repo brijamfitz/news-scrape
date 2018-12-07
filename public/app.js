c90abf4302bcb9243cc5f4fd796f8ec8c47e7da2
brijamfitz <brianjfitzgerald@gmail.com>
2018-12-07 00:32:33 -0500

    fixed dom display

--- public/app.js
+++ public/app.js
@@ -1,5 +1,14 @@
 $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
+    var newDiv = $("<div>").append(
+      $("<p>")
+        .attr("data-id", data[i]._id)
+        .text(data[i].title),
+      $("<p>").text(data[i].summary),
+      $("<p>").text(data[i].link),
+      $("<br /><br />")
+    );
+
+    $("#articles").append(newDiv);
   }
 });
