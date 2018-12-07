27d87d14ca940c924e4a97abd01a9ed6807a0a5b
brijamfitz <brianjfitzgerald@gmail.com>
2018-12-06 22:49:34 -0500

    successful scrape

--- server.js
+++ server.js
@@ -30,6 +30,31 @@ mongoose.connect(
 );
 
 // Routes
+// A GET route for scraping the news site
+axios.get('https://www.theonion.com/').then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    var results = [];
+
+    $('div.post-wrapper').each(function(i, element) {
+        var title = $(element).find('h1.headline').text();
+        var summary = $(element).find('div.excerpt').text();;
+        var link = $(element).find('a.js_entry-link').attr('href');
+
+        results.push({
+            title: title,
+            summary: summary,
+            link: link
+        });
+    });
+    console.log(results);
+});
+
+// A GET route for getting all articles from the database
+
+// A GET route for getting a specific article by id and populating with comment
+
+// A POST route for saving/updating a specific article's associated note
 
 
 
