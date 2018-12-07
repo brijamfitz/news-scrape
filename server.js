a3a7aa5c6fc3c9e07e6c2b27b0d5c0595ee25973
brijamfitz <brianjfitzgerald@gmail.com>
2018-12-06 23:15:09 -0500

    successful scrape and mongodb creation

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Set up our dependencies
-var express = require('express');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var axios = require('axios');
-var cheerio = require('cheerio');
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Require our models
-// var db = require('./models');
+var db = require("./models");
 
 // Set up our port
 var PORT = 8080;
@@ -16,38 +16,48 @@ var app = express();
 
 // Configure middleware
 // Use morgan logger for logging requests
-app.use(logger('dev'));
+app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make "public" a static folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Connect to MongoDB
 mongoose.connect(
-    'mongodb://localhost/newsScrape',
-    { useNewUrlParser: true }
+  "mongodb://localhost/newsScrape",
+  { useNewUrlParser: true }
 );
 
 // Routes
 // A GET route for scraping the news site
-axios.get('https://www.theonion.com/').then(function(response) {
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.theonion.com/").then(function(response) {
     var $ = cheerio.load(response.data);
 
-    var results = [];
+    $("div.post-wrapper").each(function(i, element) {
+      var result = {};
 
-    $('div.post-wrapper').each(function(i, element) {
-        var title = $(element).find('h1.headline').text();
-        var summary = $(element).find('div.excerpt').text();;
-        var link = $(element).find('a.js_entry-link').attr('href');
+      result.title = $(this)
+        .find("h1.headline")
+        .text();
+      result.summary = $(this)
+        .find("div.excerpt")
+        .text();
+      result.link = $(this)
+        .find("a.js_entry-link")
+        .attr("href");
 
-        results.push({
-            title: title,
-            summary: summary,
-            link: link
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          return res.json(err);
         });
     });
-    console.log(results);
+    res.send("Scrape Complete!");
+  });
 });
 
 // A GET route for getting all articles from the database
@@ -56,9 +66,7 @@ axios.get('https://www.theonion.com/').then(function(response) {
 
 // A POST route for saving/updating a specific article's associated note
 
-
-
 // Start the server
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+  console.log("App running on port " + PORT + "!");
 });
