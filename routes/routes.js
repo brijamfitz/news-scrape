20431dbecafef73abd817a42c2cb181795ff8055
brijamfitz <brianjfitzgerald@gmail.com>
2018-12-17 13:11:39 -0500

    fixed scrape route

--- routes/routes.js
+++ routes/routes.js
@@ -8,14 +8,14 @@ module.exports = function(app, cheerio, axios) {
     axios.get("https://www.theonion.com/").then(function(response) {
       var $ = cheerio.load(response.data);
 
-      $("div.post-wrapper").each(function(i, element) {
+      $("div.js_post-wrapper").each(function(i, element) {
         var result = {};
 
         result.title = $(this)
-          .find("h1.headline")
+          .find("h1.card-headline")
           .text();
         result.summary = $(this)
-          .find("div.excerpt")
+          .find("p")
           .text();
         result.link = $(this)
           .find("a.js_entry-link")
@@ -79,23 +79,23 @@ module.exports = function(app, cheerio, axios) {
   });
 
   // A DELETE route for deleting a specific article's associated comment
-  app.delete("/:id", function(req, res) {
-    console.log(req.body) // This is the title and body of comment
-    console.log(req.params.id); // This is the id of the article
-    db.Article.findOne({ _id:req.params.id }, function(err, data){
-      console.log(data.comment); // This is the comment id
-      db.Comment.findByIdAndRemove({ _id: data.comment });
-    })
-      // db.Comment.remove(req.body).then(function(dbComment) {
-      //   db.Article.findOneAndRemove(
-      //     { _id: req.params.id }
-      //   ).then(function(dbComment) {
-      //     res.json(dbComment)
-      //   }).catch(function(err) {
-      //     res.json(dbComment);
-      //   })
-      // })
-  })
+  // app.delete("/:id", function(req, res) {
+  //   console.log(req.body) // This is the title and body of comment
+  //   console.log(req.params.id); // This is the id of the article
+  //   db.Article.findOne({ _id:req.params.id }, function(err, data){
+  //     console.log(data.comment); // This is the comment id
+  //     db.Comment.findByIdAndRemove({ _id: data.comment });
+  //   })
+  //     // db.Comment.remove(req.body).then(function(dbComment) {
+  //     //   db.Article.findOneAndRemove(
+  //     //     { _id: req.params.id }
+  //     //   ).then(function(dbComment) {
+  //     //     res.json(dbComment)
+  //     //   }).catch(function(err) {
+  //     //     res.json(dbComment);
+  //     //   })
+  //     // })
+  // })
 
   // A GET route to displaying all comments
 };
